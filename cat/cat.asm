b474cb8e054e1b97eed151060f740eda93d2e074
Sven Wiltink <swiltink@transip.nl>
2020-09-16 17:33:29 +0200

    get the file to read from argv[1]

--- cat/cat.asm
+++ cat/cat.asm
@@ -2,20 +2,34 @@ section .bss
 buffer resb 512
 
 section .data
-	filename db "helloworld.txt", 0
-	open_failed db "failed to open file "
+	open_failed 		db "failed to open file", 10
+	open_failed_len		equ $-open_failed
+	no_file_provided 	db "no file provided", 10
+	no_file_provided_len 	equ $-no_file_provided
+	
 
 section .text
 
 global _start
 
 _start:
+	mov rax, [rsp]			; load argc
+	cmp rax, 2			; check if enough params provided
+	jl .no_file
+
+	;mov rax, 1			; write to
+	;mov rdi, 1			; stdout
+	;mov rdx, 12			; but only the first r13 bytes
+	;syscall
+
 	mov rax, 2			; open
-	mov rdi, filename		; file
+	mov rdi, [rsp + 16]		; filename located at argv[1]
 	mov rsi, 0			; O_RDONLY
 	syscall
+
 	cmp rax, 0
 	jl .open_failed
+
 	mov r12, rax			; store FD in r12
 
 .copy_file:
@@ -27,8 +41,8 @@ _start:
 
 	mov r13, rax			; store the amount of bytes read into r13
 
-	cmp rax, 0			; check for EOF 
-	je .exit			; exit when done
+	cmp rax, 0			; check for EOF
+	je .ok				; exit when done
 
 	mov rax, 1			; write to
 	mov rdi, 1			; stdout
@@ -42,18 +56,29 @@ _start:
 	mov rax, 1			; write
 	mov rdi, 1			; to stdout
 	mov rsi, open_failed		; open_failed
-	mov rdx, 20			; the length of the msg
+	mov rdx, open_failed_len	; the length of the msg
+
 	syscall
 	
+	jmp .fail
+
+.no_file:
 	mov rax, 1			; write
 	mov rdi, 1			; to stdout
-	mov rsi, filename		; the filename
-	mov rdx, 14			; the length of the msg
+	mov rsi, no_file_provided	; no_file_provided
+	mov rdx, no_file_provided_len	; the length of the msg
 	syscall
 
-	mov rax, 1			; set exitcode to 1
+	jmp .fail
 
+.ok:
+	mov rdi, 0
+	jmp .exit
+
+.fail:
+	mov rdi, 1
+
+; exit. Assumes rdi contains the exitcode
 .exit:
-	mov rdi, rax			; set exitcode
 	mov rax, 60			; exit
 	syscall
